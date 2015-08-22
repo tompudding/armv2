ed9fb9d31898596ec181545f6d44ff56bc2c8521
Tom Gooding <tom.pudding@gmail.com>
2015-08-23 00:18:43 +0100

    Read the entry point from the elf

--- create.py
+++ create.py
@@ -21,7 +21,9 @@ with open(sys.argv[2],'rb') as f:
 
 #Get rid of any bx lrs from the gcc stdlib
 
+entry_point = elffile.header['e_entry']
 data = data.replace(struct.pack('<I',0xe12fff1e),struct.pack('<I',0xe1a0f00e))
+boot = boot.replace(struct.pack('<I',0xcafebabe),struct.pack('<I',entry_point))
 
 assert len(boot) < 0x8000
 boot = boot + '\x00'*(0x8000 - len(boot))
