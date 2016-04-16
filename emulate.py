b510dbed177fcca97fa9f118893c2f7517729a1c
Tom Gooding <tom.pudding@gmail.com>
2016-04-16 18:19:34 +0100

    Revert "Working on werewolf"
    This reverts commit dcee68d21aef074fe5ed5a0ea46e058fce7484ad.

--- emulate.py
+++ emulate.py
@@ -15,11 +15,6 @@ from optparse import OptionParser
 width,height = (960, 720)
 globals.screen = Point(width,height)
 
-translations = {pygame.K_LEFT : 250,
-                pygame.K_RIGHT : 251,
-                pygame.K_UP : 252,
-                pygame.K_DOWN : 253}
-
 def new_machine():
     machine = hardware.Machine(cpu_size = 1<<21, cpu_rom = 'boot.rom')
     try:
@@ -99,10 +94,6 @@ class Emulator(object):
                     #use the old value
                     key = ord(event.unicode)
                 except (TypeError,AttributeError):
-                    pass
-                try:
-                    key = translations[key]
-                except KeyError:
                     pass
                 if key < 256:
                     self.dbg.machine.keyboard.KeyDown(key)
