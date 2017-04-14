f195239b6eb6d50a402b9b36a0c13f72dcc19959
Tom Gooding <tom.pudding@gmail.com>
2017-04-14 21:49:15 +0100

    Fix display issue

--- drawing/opengl.py
+++ drawing/opengl.py
@@ -151,7 +151,7 @@ def NewFrame():
     crt_buffer.BindForWriting()
     #glDepthMask(GL_TRUE)
     #glClearColor(0.0, 0.0, 0.0, 1.0)
-    #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     #glEnable(GL_DEPTH_TEST)
     #glEnable(GL_BLEND)
     #glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
