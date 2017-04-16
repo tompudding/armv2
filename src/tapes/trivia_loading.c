d5fa6890aa23a279b2b3821a7e883f9ecdcb82f7
Tom Gooding <tom.pudding@gmail.com>
2017-04-16 12:15:40 +0100

    Add a progress bar to the trivia loading

--- src/tapes/trivia_loading.c
+++ src/tapes/trivia_loading.c
@@ -11,10 +11,25 @@
 uint32_t normal   = PALETTE(BACKGROUND,FOREGROUND);
 uint32_t inverted = PALETTE(FOREGROUND,BACKGROUND);
 
+#define TITLE "Buffy the Vampire Slayer Trivia"
+
 int main(void) {
     int border_size = 2;
+    int bar_x = 4;
+    int bar_y = HEIGHT-14;
+    int bar_end = (WIDTH - bar_x);
+    int bar_width = bar_end - bar_x;
+    int title_x = (WIDTH - (sizeof(TITLE)-1)) / 2;
+    int title_y = 12;
+    uint8_t *bar_pos = letter_data + (bar_y * WIDTH) + bar_x;
+
     set_screen_data(normal, inverted, border_size);
     clear_screen_default();
+
+    bar_pos[-1] = '\x87';
+    bar_pos[bar_width] = '\x85';
+    memcpy( letter_data + (title_y * WIDTH) + title_x, TITLE, sizeof(TITLE) - 1 );
+
     uint32_t entry_point = 0;
     enum tape_codes result = tape_next_word( &entry_point );
 
@@ -38,14 +53,30 @@ int main(void) {
     uint8_t *load_area = (void*)(load_addr);
 
     uint32_t section_length = 0;
+    uint32_t num_read = 0;
     result = tape_next_word( &section_length );
     section_length &= (~TAPE_FLAG_FINAL);
-    while(result == READY && section_length != 0) {
+
+    //Now we know the length we know how many bytes we need for each blip along the progress bar
+    uint32_t bytes_per_blip = section_length / bar_width;
+    uint32_t next_blip = bytes_per_blip;
+
+    while(result == READY && section_length != num_read) {
         uint8_t byte;
         result = tape_next_byte( &byte );
         if(READY == result) {
-            section_length--;
+            num_read++;
             *load_area++ = byte;
+            next_blip--;
+
+            if(next_blip == 1) {
+                *bar_pos++ = '\x60';
+            }
+            else if(next_blip == 0) {
+                //*bar_pos++ = '\x60';
+                //*bar_pos = '\x60';
+                next_blip = bytes_per_blip;
+            }
         }
     }
     if( READY != result ) {
